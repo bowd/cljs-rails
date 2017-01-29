6f55334c55957fed3e1981d948c0b67ea55a5148
Bogdan Dumitru <dumbogdan@gmail.com>
2017-01-29 23:07:20 +0200

    Remove useless comments

--- lib/cljs/rails/helper.rb
+++ lib/cljs/rails/helper.rb
@@ -2,15 +2,7 @@ require 'action_view'
 
 module Cljs
   module Rails
-    # Asset path helpers for use with webpack
     module Helper
-      # Return asset paths for a particular webpack entry point.
-      #
-      # Response may either be full URLs (eg http://localhost/...) if the dev server
-      # is in use or a host-relative URl (eg /webpack/...) if assets are precompiled.
-      #
-      # Will raise an error if our manifest can't be found or the entry point does
-      # not exist.
       def cljs_main_path
         if ::Rails.env.production?
           cljs_main_prod_path
@@ -32,8 +24,6 @@ module Cljs
 
         host = ::Rails.configuration.cljs.dev_server.host
         host = instance_eval(&host) if host.respond_to?(:call)
-
-        # if ::Rails.env.developmtn
 
         "#{protocol}://#{host}:#{port}/#{main}.js"
       end
